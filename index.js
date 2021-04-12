bc2ecca742efec298638c83931a3ce45cd3736b5
Virac David AUK <auk.david@gmail.com>
2021-04-12 17:56:45 +0200

    Add scripts with different headers

--- index.js
+++ index.js
@@ -7,11 +7,17 @@ const COEP_REQUIRECORP = 'require-corp'
 const COOP = 'Cross-Origin-Opener-Policy'
 const COOP_SAMEORIGIN = 'same-origin'
 
-app.use(express.static('docs', {
-  setHeaders: (res) => {
-    res.set(COEP, COEP_REQUIRECORP)
-    res.set(COOP, COOP_SAMEORIGIN)
-  }
-}))
+const ARGV_NOHEADERS = '--no-headers'
+
+let setHeaders = (res) => {
+  res.set(COEP, COEP_REQUIRECORP)
+  res.set(COOP, COOP_SAMEORIGIN)
+}
+
+if (process.argv.includes(ARGV_NOHEADERS)) {
+  setHeaders = () => {}
+}
+
+app.use(express.static('docs', { setHeaders }))
 
 app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`))
